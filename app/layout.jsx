b837c25fc8c71dcab3e9e9f18402488af5c756b4
Sabit Khan <mesabitkhan@gmail.com>
2024-01-09 00:40:52 +0500

    making Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 import "@styles/globals.css";
 
 export const metadata = {
@@ -9,13 +10,15 @@ const layout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
